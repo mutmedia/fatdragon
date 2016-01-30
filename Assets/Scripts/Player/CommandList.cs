d31b75375d9130a437c036a121b49d0ba7c1e85c
Lucas Medeiros <lucamedeir@gmail.com>
2016-01-29 23:31:43 -0800

    CommandList added

---
+++ Assets/Scripts/Player/CommandList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Assets.Scripts.Player
+{
+    class CommandList
+    {
+        int CommandIndex;
+        ArrayList List;
+
+        public CommandList()
+        {
+            List = new ArrayList();
+            CommandIndex = -1;
+        }
+
+        public bool Compare(object sender, EventArgs ar)
+        {
+            bool result = false;
+            Command item = (Command)List[CommandIndex];
+            if(command.Left == item.Left && command.Right == item.Right)
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public void Add(Command command)
+        {
+            CommandIndex++;
+            List.Add(command);
+        }
+
+        public void Next()
+        {
+            CommandIndex++;
+     
+            if(CommandIndex >= List.Count)
+            {
+                CommandIndex = 0;
+            }
+        }
+    }
+}
